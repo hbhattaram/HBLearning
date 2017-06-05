c2da1ea1c2979386527482b4bda965d0592f5ce1
Harshit Bhattaram <h.bhattaram576@gmail.com>
2017-06-05 15:34:38 +0530

    Initial commit, python file

---
+++ Py_Charm_coding_files/numbers.py
@@ -0,0 +1,14 @@
+number_1 = 10
+number_2 = 20
+
+print(number_1+number_2)
+print(number_1*number_2)
+print(number_2-number_1)
+print(number_2/number_1)
+
+number_3 = 4.3
+result = (number_1*number_2)/number_3
+print(result)
+
+number_4 = result**2
+print(number_4)
